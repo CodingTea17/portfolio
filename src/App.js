c41b2df2dca616614c527f6477246293522bfc9a
Dawson M <dawson.mortenson@gmail.com>
2018-02-12 20:03:50 -0800

    add styling for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import Media from "react-media";
 import { NavBar, NavItem, Page } from './components';
 import './assests/styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
