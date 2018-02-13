856ecf0a2dd6a25eae3d71911453e64c834cc974
Dawson M <dawson.mortenson@gmail.com>
2018-02-13 14:00:53 -0800

    add backup to prevent mobilenav from displaying if it is left open and the screen size is also changed. And adds intial functions to check the status of the mouse being over the nav items

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../assests/styles/NavItem.css';
 
+const handleMouseHover = () => {
+  console.log("cool beans")
+}
+
 const NavItem = ({ title }) => {
   return (
-    <div className="NavItem">
+    <div
+        className="NavItem"
+        onMouseEnter={handleMouseHover}
+        onMouseLeave={handleMouseHover}
+    >
       { title }
     </div>
   );
